1bb92f600dab8f9566d2efe49652e97150e2061e
Hatem <hatem.deux@gmail.com>
2020-02-27 00:44:58 -0500

    public/scripts/composer-char-counter.js has been referenced in public/index.html

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,4 +3,5 @@
 console.log(`file "composer-char-counter.js" has loaded. This is its first line`);
 $(document).ready(function() {
   // --- our code goes here ---
+  console.log(`loggin from within : $(document).ready(function() {...}); .. it is working!`)
 });
