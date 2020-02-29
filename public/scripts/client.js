f872f70bfb962fcc10f6aa78646792e4a6a76449
Hatem <hatem.deux@gmail.com>
2020-02-29 12:31:24 -0500

    AJAX GET request && loadTweets() function implemented. hardcoded database commented out. (to be deleted later)

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,3 +1,5 @@
+// import { json } from "body-parser";
+
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
@@ -46,30 +48,30 @@ return markup;
 // const $tweet = createTweetElement(tweetData);
 
 // Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
 
 const renderTweets = function (arrayOfTweetObjs) { /* tweets */
   // appending each tweet to the #tweets-container
@@ -82,8 +84,24 @@ const renderTweets = function (arrayOfTweetObjs) { /* tweets */
     $('#tweets-container').append($tweet);
   }
 }
-$(document).ready(function() {
-  renderTweets(data);
+// $(document).ready(function() {
+//   // renderTweets(data);
+//   });
+
+
+  $(function() {
+    const loadTweets = function() { 
+      $.ajax({
+        method: 'GET',
+        url:'/tweets'
+      
+      })
+      .then(function(tweetData) {
+        console.log('tweetData is : ', tweetData);
+        renderTweets(tweetData);
+      }   
+      )};
+      loadTweets();
   });
 
 $(function(){
@@ -101,10 +119,11 @@ $(function(){
       url:'/tweets',
       data: $tweetText,
     })
-    .then(function (tweetPost) {
+    .then(function (tweetPost) { //the callback argument value from the ajax request.
       
-      console.log('Success: ', tweetPost);
+    console.log('Success: ', tweetPost);
       // $submitTweetButton.replaceWith(tweetPost);
     });
   });
 });
+
