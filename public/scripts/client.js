84f88819a0a7beb8a8456232ab5e8089cdcd62bb
Hatem <hatem.deux@gmail.com>
2020-03-02 01:31:46 -0500

    Code Clean up: removing deadcode, adding clarifing and title comments , linting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -11,23 +11,21 @@ const maxTweetChar = 140;
 
 //HELPER FUNCTION: GET DATE AGE of a tweet
 const getTweetAge = function(tweetCreateTime) {
-  let date = (Date.now() - tweetCreateTime)/(1000*60*60*24)
+  let date = (Date.now() - tweetCreateTime) / (1000 * 60 * 60 * 24);
   if (date < 1) {
-    return Math.floor(date*24) + " hrs ago";
-  } else return Math.floor(date) + " days ago"
-}
+    return Math.floor(date * 24) + " hrs ago";
+  } else return Math.floor(date) + " days ago";
+};
 
-
-const createTweetElement = function (tweetData) {
-  // const $tweet = $("<section>").addClass("tweet");
-  // document.createTextNode(str)
-  console.log('tweetData = ', tweetData)
-  console.log('tweetData.content.text =', tweetData.content.text)
+// HELPER/ INIT FUNCTION: creates new tweet HTML elements, feedsinto renderTweets().
+const createTweetElement = function(tweetData) {
+  console.log('tweetData = ', tweetData);
+  console.log('tweetData.content.text =', tweetData.content.text);
   const $tweet = $('<article>').text(tweetData.content.text);
-  console.log('$tweet = ',$tweet)
-  console.log('$tweet[0].innerHTML = ', $tweet[0].innerHTML)
-  const dateAgo = getTweetAge(tweetData.created_at)
-  const rightSideFooter = 
+  console.log('$tweet = ',$tweet);
+  console.log('$tweet[0].innerHTML = ', $tweet[0].innerHTML);
+  const dateAgo = getTweetAge(tweetData.created_at);
+  const rightSideFooter =
   `<img src="/images/flagIcon.png" alt="" height="15px" width="15px"> 
   <img src="/images/retweetIcon.png" alt="" height="15px" width="15px"> 
   <img src="/images/heartIcon.png" alt="" height="15px" width="15px"> 
@@ -40,7 +38,7 @@ const createTweetElement = function (tweetData) {
   </header>
 
 <article>`
-+$tweet[0].innerHTML+ 
++ $tweet[0].innerHTML +
 `</article>
 
   <footer>
@@ -48,143 +46,91 @@ const createTweetElement = function (tweetData) {
     <div>${dateAgo}</div><div class="RSF">${rightSideFooter}</div>
   </footer>
 </section>
-  `
-/*
- <article>
-    ${tweetData.content.text}
-    ${document.createTextNode(tweetData.content.text)['text']}
-  </article>
-*/
+  `;
 
-return markup;
-// return tweet <article>
-}
+  return markup;
+};
 
-
-
-
-// Test / driver code (temporary). Eventually will get this from the server.
-/* const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
- */
-// const $tweet = createTweetElement(tweetData);
-
-// Fake data taken from initial-tweets.json
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
-
-const renderTweets = function (arrayOfTweetObjs) { /* tweets */
+// FUNCTION: render tweets from database to the page.
+const renderTweets = function(arrayOfTweetObjs) { /* tweets */
   $('#tweets-container').empty();
   // appending each tweet to the #tweets-container
   // loops through tweets
-  for ( let tweetObj of arrayOfTweetObjs) {
+  for (let tweetObj of arrayOfTweetObjs) {
     console.log(tweetObj);
     // calls createTweetElement for each tweet
     // takes return value and appends it to the tweets container
     let $tweet = createTweetElement(tweetObj);
     $('#tweets-container').prepend($tweet);
   }
-}
-// $(document).ready(function() {
-//   // renderTweets(data);
-//   });
+};
 
-const loadTweets = function() { 
+// FUNCTION: ASYNC/AJAX : Get request from database
+const loadTweets = function() {
   $.ajax({
     method: 'GET',
     url:'/tweets'
   
   })
-  .then(function(tweetData) {
-    console.log('tweetData is : ', tweetData);
-    renderTweets(tweetData);
-  }   
-  )};
+    .then(function(tweetData) {
+      console.log('tweetData is : ', tweetData);
+      renderTweets(tweetData);
+    }
+    );
+};
 
-  $(function() {
-      loadTweets();
-  });
+// calling loadTweets
+$(function() {
+  loadTweets();
+});
 
-$(function(){
+// TWEET user submition logic
+$(function() {
   $('.new-tweet').hide();
   $('.tweetValidationError').hide();
 
   const $submitTweetButton = $('.new-tweet').find('input');
   console.log($submitTweetButton);
   $submitTweetButton.on('click', function(event) {
-    console.log('EVENT OF PREV',event)
+    console.log('EVENT OF PREV',event);
     console.log('New Tweet Button clicked, performing ajax call...');
     event.preventDefault();
-    // console.log('HEEERE, UNSURREAL', $(this).parents('form') );
-    console.log('HEEERE, SURREAL', $(this).parents('form').serialize() );
-    const $tweetText = $(this).parents('form').serialize() ;
-    // VALIDATION: tweetbox can not be null or ex
-    // debugger;
-    // NOTE TO FUTURE SELF: LEAVING "text=" AS IS COULD BE PROBLEMATIC IN THE FUTURE, YOUR PRESENT.
-    if ($tweetText === "text=" ) {
-      // alert("The Tweet Box can not be empty.");
-      $('.tweetValidationError').slideDown(1000)
+    const $tweetText = $(this).parents('form').serialize();
+
+    // VALIDATIONS: Empty box or max char exceeded, followed by logic POST REQ
+
+    if ($tweetText === "text=") {
+      // NOTE TO FUTURE SELF: LEAVING "text=" AS IS COULD BE PROBLEMATIC IN THE FUTURE, YOUR PRESENT.
+      $('.tweetValidationError').slideDown(1000);
       $('.tweetValidationError > div').html("The Tweet Box can not be empty.");
 
     } else if ($tweetText.length > maxTweetChar) {
       // alert(`The Tweet box can not exceed ${maxTweetChar} characters`);
       $('.tweetValidationError').slideDown(1000);
-      $('.tweetValidationError > div').text(`The Tweet box can not exceed ${maxTweetChar} characters`)
+      $('.tweetValidationError > div').text(`The Tweet box can not exceed ${maxTweetChar} characters`);
       // maxTweetChar is on top, in "GLOBAL VARIABLES".
     } else {
-      $('.tweetValidationError').slideUp(1000)
-      $.ajax( { 
+      $('.tweetValidationError').slideUp(1000);
+      $.ajax({
         method: 'POST',
         url:'/tweets',
         data: $tweetText,
       })
-      .then(function () { 
-      $('.new-tweet textarea').val('');
-      $('.new-tweet').find('span.counter').text(maxTweetChar);
-      loadTweets();
-      console.log('Success: ');
-        // $submitTweetButton.replaceWith(tweetPost);
-        // $('#tweets-container').append(tweetPost);
-      });
+        .then(function() {
+          $('.new-tweet textarea').val('');
+          $('.new-tweet').find('span.counter').text(maxTweetChar);
+          loadTweets();
+          console.log('Success: ');
+        });
     }
 
   });
 });
 
-$(function(){
+// NAV BAR event listening and functionality.
+$(function() {
   const writeNewTweetButton = $('nav > button');
-  writeNewTweetButton.on('click', function(event){
+  writeNewTweetButton.on('click', function(event) {
     $('.new-tweet').slideToggle(1000);
-  } )
-})
+  });
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -10,27 +10,24 @@ const countsRemainingChar = function(textToCount, maxCharCount) {
   let remainCharCount = maxCharCount - textToCount.length;
   console.log(`remaining chars: ${remainCharCount}`);
   return remainCharCount;
-}
+};
 
 
 $(document).ready(function() {
   // --- our code goes here ---
-  console.log(`loggin from within : $(document).ready(function() {...}); .. it is working!`)
+  console.log(`loggin from within : $(document).ready(function() {...}); .. it is working!`);
 
   // currenttly using event = "keydown" for responsiveness. However it doesn's handle copy/paste without a keypress.
   // Event = "change" works too, handles copy/paste, yet take a while to respond and I'm unsure what specifically fires it.
-  $("textarea").on( "keydown", function() {
-    // console.log(this);
-    // console.log(`Text Input: ${$(this).val()}`);
+
+  $("textarea").on("keydown", function() {
     let count = countsRemainingChar($(this).val(), 140);
-    // console.log($(this).sibling().get( 0 ).tagName )
-    const form = $(this).parent().get( 0 ).tagName 
+    const form = $(this).parent().get(0).tagName;
   
     if (count >= 0) {
       $(form).find($(".counter")).text(count).css("color", "inherit");
     } else {
       $(form).find($(".counter")).text(count).css("color", "red");
     }
-    // $(span + ".counter").text(count);
-  } );
+  });
 });
